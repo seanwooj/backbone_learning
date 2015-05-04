c4fac8d9faed416cdd2aaf8e08775af57e2b8ad8
seanwooj <seanwoojinkim@gmail.com>
2015-05-04 14:52:23 -0700

    add static sitecontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => "site#root"
+
   namespace :api do
     resources :todos, :only => [:create, :index, :show] do
       resources :comments, :only => [:index]
