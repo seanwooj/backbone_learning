3aaa37229cd1a50ff4a061f9d94101e979a21912
seanwooj <seanwoojinkim@gmail.com>
2015-05-04 14:45:04 -0700

    api layer built

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,27 @@
+class Api::TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    render :json => @todos
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render :json => @todo
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render :json => @todo
+    else
+      render :json => @todo.errors, :status => :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params[:todo].permit(:title)
+  end
+end
